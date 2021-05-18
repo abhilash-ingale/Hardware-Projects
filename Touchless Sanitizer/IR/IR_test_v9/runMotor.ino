0048f5143bde249aa298f2456da7c67f1f197ba3
Abhilash Ingale <abhilashingale08@gmail.com>
2021-05-18 12:20:48 +0200

    Add files via upload

---
+++ Touchless Sanitizer/IR/IR_test_v9/runMotor.ino
@@ -0,0 +1,21 @@
+
+void runMotor()
+{
+
+  set_time = analogRead(time_pot);
+  set_time = map(set_time, 0, 1023, tON_motor_min, tON_motor_max);
+  digitalWrite(motorPin, HIGH);
+  delay(set_time);
+  digitalWrite(motorPin, LOW);
+  delay(false_trig_delay);
+
+  while(true)
+  {
+   if (DetectHand()==false)
+   {
+    break;
+   }
+   delay(200);   
+  }
+  
+}
